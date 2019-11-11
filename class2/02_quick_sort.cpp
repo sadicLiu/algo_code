1f1104073c773fc10c56274781645c753ddcf343
sadicLiu <190978752@qq.com>
2019-11-11 15:12:29 +0800

    class 5 code done

--- class2/02_quick_sort.cpp
+++ class2/02_quick_sort.cpp
@@ -2,52 +2,58 @@
 
 using namespace std;
 
-void swap(int *arr, int i, int j) {
+void swap(int *arr, int i, int j)
+{
     int tmp = arr[i];
     arr[i] = arr[j];
     arr[j] = tmp;
 }
 
-void partition(int *arr, int l, int r, int num, int *res) {
+void partition(int *arr, int l, int r, int num, int *res)
+{
     int min = l - 1, max = r + 1, cur = l;
-    while (cur < max) {
-        if (arr[cur] < num) {
+    while (cur < max)
+    {
+        if (arr[cur] < num)
             swap(arr, ++min, cur++);
-        } else if (arr[cur] == num) {
+        else if (arr[cur] == num)
             cur++;
-        } else {
+        else
             swap(arr, cur, --max);
-        }
     }
 
     res[0] = min + 1, res[1] = max - 1;
 }
 
-void quick_sort_process(int *arr, int l, int r) {
+void quicksortProcess(int *arr, int l, int r)
+{
     if (arr == nullptr || l >= r)
         return;
 
     int res[2];
     partition(arr, l, r, arr[r], res);
-    quick_sort_process(arr, l, res[0] - 1);
-    quick_sort_process(arr, res[1] + 1, r);
+    quicksortProcess(arr, l, res[0] - 1);
+    quicksortProcess(arr, res[1] + 1, r);
 }
 
-void quick_sort(int *arr, int length) {
+void quicksort(int *arr, int length)
+{
     if (arr == nullptr || length < 2)
         return;
 
-    quick_sort_process(arr, 0, length - 1);
+    quicksortProcess(arr, 0, length - 1);
 }
 
 
-int main() {
+int main()
+{
 //    int arr[] = {3, 4, 1, 5, 6, 2, 8};
 //    int arr[] = {5, 4, 3, 2, 1};
 //    int arr[] = {1, 2, 3, 4, 5};
     int arr[] = {3, 2, 1, 5, 6, 4, 3, 9};
     quick_sort(arr, sizeof(arr) / sizeof(int));
-    for (int i = 0; i < sizeof(arr) / sizeof(int); ++i) {
+    for (int i = 0; i < sizeof(arr) / sizeof(int); ++i)
+    {
         cout << arr[i] << " ";
     }
 
--- class5/10_NodeNumber.cpp
+++ class5/10_NodeNumber.cpp
@@ -1,11 +1,59 @@
 #include <iostream>
+#include <BinaryTreeUtils.h>
+#include <cmath>
 
 using namespace std;
 
 // 已知一棵完全二叉树, 求其节点个数, 要求时间复杂度低于O(n)
 
-int main() {
+int getHeight(Node *root)
+{
+    int height = 0;
+    Node *p = root;
+    while (p != nullptr)
+    {
+        height++;
+        p = p->left;
+    }
 
+    return height;
+}
+
+int getNodeNumber(Node *root)
+{
+    if (root == nullptr)
+        return 0;
+
+    int height = getHeight(root);
+    int rightHeight = getHeight(root->right);
+    if (rightHeight == height - 1)  // 左子树为完全二叉树
+    {
+        return pow(2, height - 1) + getNodeNumber(root->right);
+    } else  // rightHeight == height - 2, 右子树为完全二叉树
+    {
+        return pow(2, rightHeight) + getNodeNumber(root->left);
+    }
+}
+
+int main()
+{
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right = new Node(3);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    root->left->left->left = new Node(8);
+//    printTree(root);
+
+    cout << getNodeNumber(root) << endl;
+
+    root->left->left->right = new Node(9);
+    root->left->right->left = new Node(10);
+    root->left->right->right = new Node(11);
+    root->right->left->left = new Node(12);
+    cout << getNodeNumber(root) << endl;
 
     return 0;
 }
