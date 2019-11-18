d36eecfd1a788c84f696a078e31c3d9525b704dc
sadicLiu <190978752@qq.com>
2019-11-18 14:59:47 +0800

    add concat string code

--- class7/04_ConcatString.cpp
+++ class7/04_ConcatString.cpp
@@ -4,28 +4,38 @@
 
 using namespace std;
 
+// 给定一些字符串, 将他们拼接成一个字符串,
+
 bool compare(const string s1, const string s2)
 {
     return (s1 + s2) < (s2 + s1);
 }
 
-int main()
+string concatString(vector<string> &strs)
 {
-    string s1 = "b";
-    string s2 = "bc";
-    string s3 = "ab";
-    vector<string> strings;
-    strings.push_back(s1);
-    strings.push_back(s2);
-    strings.push_back(s3);
-    sort(strings.begin(), strings.end(), compare);
+    sort(strs.begin(), strs.end(), compare);
 
-    for (int i = 0; i < strings.size(); ++i)
+    string res = "";
+    for (int i = 0; i < strs.size(); ++i)
     {
-        cout << strings[i] << endl;
+        res += strs[i];
     }
 
+    return res;
+}
 
+int main()
+{
+    vector<string> strs;
+    strs.push_back("b");
+    strs.push_back("ba");
+    cout << concatString(strs) << endl;
+
+    strs.clear();
+    strs.push_back("ab");
+    strs.push_back("ac");
+    strs.push_back("ba");
+    cout << concatString(strs) << endl;
 
     return 0;
 }
