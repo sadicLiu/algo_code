83eb69860b64a08c0a6d6cf03734e7c4df115bec
sadicLiu <190978752@qq.com>
2019-12-26 08:09:55 +0800

    refactor 122

--- nc/class3/06_122.cpp
+++ nc/class3/06_122.cpp
@@ -17,35 +17,27 @@ public:
         for (int i = 0; i < height.size(); i++)
         {
             int curHeight = height[i];
-
-            // 等于的情况直接把下标往栈里放没关系, 因为结算栈中靠上的等于元素时, 它的面积计算结果虽然是错误的
-            // 但是它下面的元素结算的结果肯定比它大, 所以这个错误结果不会成为最终结果
-            if (myStack.empty() || curHeight >= height[myStack.top()])
-                myStack.push(i);
-            else if (curHeight < height[myStack.top()])
+            if (!myStack.empty() && curHeight < height[myStack.top()])
             {
                 while (!myStack.empty() && curHeight < height[myStack.top()])
-                {
-                    int topIndex = myStack.top();
-                    myStack.pop();
-                    int down = myStack.empty() ? -1 : myStack.top();
-                    int curArea = (i - down - 1) * height[topIndex];
-                    res = curArea > res ? curArea : res;
-                }
-
-                myStack.push(i);
+                    update(res, myStack, height, i);
             }
+
+            myStack.push(i);
         }
 
         while (!myStack.empty())
-        {
-            int topIndex = myStack.top();
-            myStack.pop();
-            int down = myStack.empty() ? -1 : myStack.top();
-            int curArea = (height.size() - down - 1) * height[topIndex];
-            res = curArea > res ? curArea : res;
-        }
+            update(res, myStack, height, height.size());
 
         return res;
+    }
+
+    void update(int &res, stack<int> &myStack, vector<int> &height, int right)
+    {
+        int topIndex = myStack.top();
+        myStack.pop();
+        int left = myStack.empty() ? -1 : myStack.top();
+        int area = (right - left - 1) * height[topIndex];
+        res = area > res ? area : res;
     }
 };
